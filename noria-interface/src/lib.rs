2724503f3622dfba6e5e122d2938feaffd9dd731
Justus Adam <dev@justus.science>
2022-09-07 04:16:42 +0000

    Untested draft for Rust query interface

--- noria-interface/src/lib.rs
+++ noria-interface/src/lib.rs
@@ -3,17 +3,25 @@ use std::ffi;
 extern crate noria;
 extern crate tokio;
 
-#[repr(transparent)]
+use std::rc::Rc;
+use std::collections::HashMap;
+
+use std::os::raw::{c_int, c_char};
+
 pub struct Connection(noria::SyncControllerHandle<noria::LocalAuthority, tokio::runtime::TaskExecutor>);
 
-#[repr(transparent)]
-pub struct Row(Vec<noria::DataType>);
+pub struct Row(Vec<noria::DataType>, Schema);
 
-#[repr(transparent)]
-pub struct QueryResult(std::vec::IntoIter<Vec<noria::DataType>>);
+pub struct QueryResult(std::vec::IntoIter<Vec<noria::DataType>>, Schema);
+
+type Schema = Rc<HashMap<String, usize>>;
+
+fn new_schema(columns: &[String]) -> Schema {
+    Rc::new(columns.iter().cloned().enumerate().map(|(a, b)| (b, a)).collect())
+}
 
 #[no_mangle]
-pub unsafe extern "C" fn setup_connection(dat_file: *const std::os::raw::c_char) -> Box<Connection> {
+pub unsafe extern "C" fn setup_connection(dat_file: *const c_char) -> Box<Connection> {
     let s = ffi::CStr::from_ptr(dat_file);
     println!("Recieved filename {}", s.to_str().unwrap());
     let mut b = noria::Builder::default();
@@ -22,17 +30,25 @@ pub unsafe extern "C" fn setup_connection(dat_file: *const std::os::raw::c_char)
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn run_query(mut conn: Box<Connection>, q: *const std::os::raw::c_char, key: std::os::raw::c_int) -> Box<QueryResult> {
-    let res = conn.0.view(ffi::CStr::from_ptr(q).to_str().unwrap()).unwrap().into_sync().lookup(&[key.into()], true).unwrap();
-    Box::new(QueryResult(res.into_iter()))
+pub unsafe extern "C" fn run_query(conn: &mut Connection, q: *const c_char, key: c_int) -> Box<QueryResult> {
+    let mut view = conn.0.view(ffi::CStr::from_ptr(q).to_str().unwrap()).unwrap().into_sync();
+    let res = view.lookup(&[key.into()], true).unwrap();
+    Box::new(QueryResult(res.into_iter(), new_schema(view.columns())))
 }
 
 #[no_mangle]
-pub extern "C" fn next_row(mut result: Box<QueryResult>) -> Option<Box<Row>> {
-    result.0.next().map(|i| Box::new(Row(i)))
+pub extern "C" fn next_row(result: &mut QueryResult) -> Option<Box<Row>> {
+    result.0.next().map(|i| Box::new(Row(i, result.1.clone())))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn result_get_int(row: Box<Row>) -> std::os::raw::c_int {
-    0
+pub unsafe extern "C" fn row_get_int(row: &Row, key: *const c_char) -> c_int {
+    row.0[row.1[ffi::CStr::from_ptr(key).to_str().unwrap()]].clone().into()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn row_get_string(row: &Row, key: *const c_char) -> *mut c_char {
+    let string : String = row.0[row.1[ffi::CStr::from_ptr(key).to_str().unwrap()]].clone().into();
+    let cstring = ffi::CString::new(string).unwrap();
+    cstring.into_raw()
 }
